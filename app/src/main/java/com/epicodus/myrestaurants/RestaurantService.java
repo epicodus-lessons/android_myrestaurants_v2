b1e6e045f59a121776ed579adea2126d8019004e
Courtney Phillips <courtney@epicodus.com>
2017-05-26 10:37:27 -0700

    Build new instance of OkHttpClient to manage API requests.

--- app/src/main/java/com/epicodus/myrestaurants/RestaurantService.java
+++ app/src/main/java/com/epicodus/myrestaurants/RestaurantService.java
@@ -1,12 +1,13 @@
 package com.epicodus.myrestaurants;
 
 import okhttp3.Callback;
+import okhttp3.OkHttpClient;
 
 public class RestaurantService {
     static String apiUrl = "http://138.197.214.133/api/v1/foodtruck";
 
     public static void findRestaurants(Callback callback) {
-
+        OkHttpClient client = new OkHttpClient.Builder().build();
     }
 
 }
