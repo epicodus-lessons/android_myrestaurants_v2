2ba5d86d8b0c64ba4b26e077cb9edaec63dfd92c
Courtney Phillips <courtney@epicodus.com>
2017-05-26 11:38:00 -0700

    Populate RestaurantListAdapter class.

--- app/src/main/java/com/epicodus/myrestaurants/adapters/RestaurantListAdapter.java
+++ app/src/main/java/com/epicodus/myrestaurants/adapters/RestaurantListAdapter.java
@@ -1,5 +1,18 @@
 package com.epicodus.myrestaurants.adapters;
 
 
-public class RestaurantListAdapter {
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import com.epicodus.myrestaurants.models.Restaurant;
+import java.util.ArrayList;
+
+public class RestaurantListAdapter extends RecyclerView.Adapter<RestaurantListAdapter.RestaurantViewHolder> {
+    private ArrayList<Restaurant> mRestaurants = new ArrayList<>();
+    private Context mContext;
+
+    public RestaurantListAdapter(Context context, ArrayList<Restaurant> restaurants) {
+        mContext = context;
+        mRestaurants = restaurants;
+    }
+
 }
