b8382009af1dda71089c0e4677d6d692cc0da2ce
Courtney Phillips <courtney@epicodus.com>
2017-05-26 10:36:11 -0700

    Add API URL as static variable for use in RestaurantService class.

--- app/src/main/java/com/epicodus/myrestaurants/RestaurantService.java
+++ app/src/main/java/com/epicodus/myrestaurants/RestaurantService.java
@@ -1,7 +1,9 @@
 package com.epicodus.myrestaurants;
+
 import okhttp3.Callback;
 
 public class RestaurantService {
+    static String apiUrl = "http://138.197.214.133/api/v1/foodtruck";
 
     public static void findRestaurants(Callback callback) {
 
