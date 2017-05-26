dbfcf83fbb3bd4e18c77c715ac339d03a4c3d266
Courtney Phillips <courtney@epicodus.com>
2017-05-26 10:38:20 -0700

    Build Request object to contact API.

--- app/src/main/java/com/epicodus/myrestaurants/RestaurantService.java
+++ app/src/main/java/com/epicodus/myrestaurants/RestaurantService.java
@@ -2,12 +2,16 @@ package com.epicodus.myrestaurants;
 
 import okhttp3.Callback;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
 
 public class RestaurantService {
     static String apiUrl = "http://138.197.214.133/api/v1/foodtruck";
 
     public static void findRestaurants(Callback callback) {
         OkHttpClient client = new OkHttpClient.Builder().build();
+        Request request= new Request.Builder()
+                .url(apiUrl)
+                .build();
     }
 
 }
