1ab4d7b58d6967023baeab3f3bfd3f26705f9c03
Courtney Phillips <courtney@epicodus.com>
2017-05-26 10:56:15 -0700

    Define processResults() method in RestaurantService to parse JSON responses, and construct into Restaurant objects using new data model.

--- app/src/main/java/com/epicodus/myrestaurants/RestaurantService.java
+++ app/src/main/java/com/epicodus/myrestaurants/RestaurantService.java
@@ -1,9 +1,17 @@
 package com.epicodus.myrestaurants;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
 import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Call;
+import okhttp3.Response;
 
 public class RestaurantService {
     static String apiUrl = "http://138.197.214.133/api/v1/foodtruck";
@@ -15,6 +23,34 @@ public class RestaurantService {
                 .build();
         Call call = client.newCall(request);
         call.enqueue(callback);
+    }
+
+    public ArrayList<Restaurant> processResults(Response response) {
+        ArrayList<Restaurant> restaurants = new ArrayList<>();
+        try {
+            String jsonData = response.body().string();
+            if (response.isSuccessful()) {
+                JSONArray restaurantsJSON = new JSONArray(jsonData);
+                for (int i = 0; i < restaurantsJSON.length(); i++) {
+                    JSONObject restaurantJSON = restaurantsJSON.getJSONObject(i);
+                    String name = restaurantJSON.getString("name");
+                    double averageCost = restaurantJSON.getDouble("avgcost");
+                    int rating = Integer.parseInt(restaurantJSON.getString("rating"));
+                    String website = restaurantJSON.getString("website");
+                    String imageUrl = restaurantJSON.getString("image");
+                    String foodType = restaurantJSON.getString("foodtype");
+
+                    Restaurant restaurant = new Restaurant(name, averageCost, rating, website,
+                            imageUrl, foodType);
+                    restaurants.add(restaurant);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return restaurants;
     }
 
 }
