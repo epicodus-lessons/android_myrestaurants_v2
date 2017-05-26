b604eb31d271f8eebf31f2227d13dbc4ce38eb48
Courtney Phillips <courtney@epicodus.com>
2017-05-26 10:39:01 -0700

    Add code to execute API request with Call object.

--- app/src/main/java/com/epicodus/myrestaurants/RestaurantService.java
+++ app/src/main/java/com/epicodus/myrestaurants/RestaurantService.java
@@ -3,6 +3,7 @@ package com.epicodus.myrestaurants;
 import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.Call;
 
 public class RestaurantService {
     static String apiUrl = "http://138.197.214.133/api/v1/foodtruck";
@@ -12,6 +13,8 @@ public class RestaurantService {
         Request request= new Request.Builder()
                 .url(apiUrl)
                 .build();
+        Call call = client.newCall(request);
+        call.enqueue(callback);
     }
 
 }
