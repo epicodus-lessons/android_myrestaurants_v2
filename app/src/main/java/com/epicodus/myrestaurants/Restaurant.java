2204c38ce90a414c6ad91e4183295a270e673458
Courtney Phillips <courtney@epicodus.com>
2017-05-26 10:54:31 -0700

    Populate Restaurant model with member variables, constructor, and getter methods.

--- app/src/main/java/com/epicodus/myrestaurants/Restaurant.java
+++ app/src/main/java/com/epicodus/myrestaurants/Restaurant.java
@@ -2,4 +2,45 @@ package com.epicodus.myrestaurants;
 
 
 public class Restaurant {
+    private String mName;
+    private double mAverageCost;
+    private int mRating;
+    private String mWebsite;
+    private String mImageUrl;
+    private String mFoodType;
+
+    public Restaurant(String name, double averageCost, int rating,
+                      String website, String imageUrl, String foodType) {
+        this.mName = name;
+        this.mAverageCost = averageCost;
+        this.mRating = rating;
+        this.mWebsite = website;
+        this.mImageUrl = imageUrl;
+        this.mFoodType = foodType;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public double getAverageCost() {
+        return mAverageCost;
+    }
+
+    public String getWebsite() {
+        return  mWebsite;
+    }
+
+    public double getRating() {
+        return mRating;
+    }
+
+    public String getImageUrl(){
+        return mImageUrl;
+    }
+
+    public String getFoodType(){
+        return mFoodType;
+    }
+
 }
